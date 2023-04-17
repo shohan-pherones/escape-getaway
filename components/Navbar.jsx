7cf5638dfbc67763a2af60d070dccf4986967bbd
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-04-17 15:26:51 +0600

    logo and nav issue solve

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,19 +6,17 @@ const Navbar = () => {
   return (
     <div>
       <div className="container mx-auto flex h-[4rem] md:h-[5rem]  items-center justify-between">
-        <div className="logo text-3xl font-semibold text-cyan-500">
-          <Link href="/">
-            Esxape- <span className="italic">getaway</span>
-          </Link>
+        <div className="logo text-3xl font-bold text-cyan-500">
+          <Link href="/">Esxape-getaway</Link>
         </div>
         <div className="nav-links">
           <ul className="flex gap-3 md:gap-8 items-center justify-center">
             <li>
               <Link
-                href=""
+                href="/"
                 className="hover:text-cyan-600 font-semibold duration-300"
               >
-                About
+                Home
               </Link>
             </li>
             <li>
