0b035367dba06ada4565415d11f49bd6afef645c
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-18 21:01:43 +0600

    checkout ssr added

--- pages/checkout/[packageId].js
+++ pages/checkout/[packageId].js
@@ -1,5 +1,30 @@
-const Checkout = () => {
-  return <div>Checkout</div>;
+import { getAPackage } from "@/prisma/packages";
+
+const Checkout = ({ singlePackage }) => {
+  return (
+    <div>
+      {/* TEST */}
+      <p>{singlePackage?.title}</p>
+    </div>
+  );
 };
 
 export default Checkout;
+
+/* DO NOT TOUCH THESE CODES */
+export const getServerSideProps = async ({ query }) => {
+  const singlePackage = await getAPackage(query.packageId);
+
+  const updatedPackage = {
+    ...singlePackage,
+    updatedAt: singlePackage.updatedAt.toString(),
+    createdAt: singlePackage.createdAt.toString(),
+  };
+
+  return {
+    props: {
+      singlePackage: updatedPackage,
+    },
+  };
+};
+/* DO NOT TOUCH THESE CODES */
